d1183e8e1401d9008e6e0fd6e0cadb04d074e9a8
evansanhueza <evan.sanhu@gmail.com>
2024-09-19 17:54:22 -0400

    Arranger des petits détails

--- portfolio_theme/header.php
+++ portfolio_theme/header.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/dist/styles/main.css" />
 
     <script>
-      // À décommenter lorsque rendu dans le fichier .php pour que les icônes fonctionnent dans WP
        iconsPath = '<?php bloginfo('template_url') ?>/dist/';
     </script>
     <script src="<?php bloginfo('template_url'); ?>/dist/scripts/main.js" defer></script>
@@ -17,26 +16,8 @@
   </head>
 
   <body <?php body_class(); ?>>
-    <header class="header" data-component="Header">
+    <header class="header" >
      
-      <a href="<?php bloginfo('url'); ?>" class="brand">
-          <svg class="icon icon--md">
-            <use xlink:href="#icon-logo"></use>
-          </svg>
-          <?php bloginfo('name'); ?>
-      </a>
-
-      <?php wp_nav_menu(array(
-          'theme_location' => 'menu_principal',
-          'container' => 'nav',
-          'menu_class' => 'nav__menu',
-      )); ?>
-
-
-    <button class="header__toggle js-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-    </button>
+      
 
   </header>
