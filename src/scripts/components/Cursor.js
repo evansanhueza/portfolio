799793fe0593a4c5c6f6d77ac282bf88927150b7
evansanhueza <evan.sanhueza@gmail.com>
2024-10-18 17:58:19 -0400

    Arranger petits détails

--- src/scripts/components/Cursor.js
+++ src/scripts/components/Cursor.js
@@ -3,8 +3,6 @@ export default class Cursor {
     this.init();
   }
   init() {
-    // UPDATE: I was able to get this working again... Enjoy!
-
     var cursor = document.querySelector('.cursor');
     var cursorinner = document.querySelector('.cursor2');
     var a = document.querySelectorAll('a');
@@ -13,11 +11,6 @@ export default class Cursor {
       var x = e.clientX;
       var y = e.clientY;
       cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
-    });
-
-    document.addEventListener('mousemove', function (e) {
-      var x = e.clientX;
-      var y = e.clientY;
       cursorinner.style.left = x + 'px';
       cursorinner.style.top = y + 'px';
     });
